82a5679ebd30aa99dffda01e0a67c02a395f82ae
Jesse Merritt <jes150@gmail.com>
2013-02-11 00:34:25 -0500

    Added License information Added comments to the Arduino sketch Added CAD for gears Added GCode for gears

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 servospindle
 ============
+Written by Jesse Merritt www.github.com/jes1510 February 5, 2013
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/
 
 Converts a manual router speed controller to CNC control by connecting the potentiometer of the controller to a servo.  The servo is controlled by an Arduino board connected to the parallel port.  The Arduino decodes the PWM from the computer into a servo position.
--- spindle_controller.ino
+++ spindle_controller.ino
@@ -1,19 +1,31 @@
-// Spindle speed control using servo controlled speed controller
-// Based on the servo 'Knob' example in Arduino
-// Jes1510@gmail.com
+/* Written by Jesse Merritt www.github.com/jes1510 February 5, 2013
 
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/
+
+
+Spindle speed control using servo controlled speed controller
+Based partially on the servo 'Knob' example in Arduino.
+Requires Arduino IDE v 1.0.3 or greater
+
+/**/
 
 #include <Servo.h> 
  
 Servo speedServo;        // create servo object to control a servo 
  
-int potPin = 0;      // analog pin used to connect the potentiometer
-int modeSwitch = 2;  // Mode switch on pin 2
-int pwmInputPin = 3;
-int ledPin = 13;     // Indicator LED for mode
+int potPin = 0;          // analog pin used to connect the potentiometer
+int modeSwitch = 2;      // Mode switch on pin 2
+int pwmInputPin = 3;     // Pin connected to parallel port on the computer
+int ledPin = 13;         // Indicator LED for mode indicator
 
-unsigned long  pulseHighWidth ;    // How wide High portion of pulse 
-int val;              // variable to read the value from the analog pin 
+unsigned long  pulseHighWidth ;     // Measure width of incoming pulse. Default PWM is 100Hz
+int val;                            // variable to hold the value from the analog pin 
 
 
 int mode = 0;        // Init mode to manual
@@ -27,59 +39,61 @@ int pwm = 1;          // PWM mode
  
 void setup() 
 { 
-  speedServo.attach(9);                        // Servo on Pin 9
-  pinMode(modeSwitch, INPUT_PULLUP);        // Switch for mode, pullup on
-  Serial.begin(9600);                                  
+  speedServo.attach(9);                      // Servo on Pin 9
+  pinMode(modeSwitch, INPUT_PULLUP);         // Switch for mode, pullup on
+  Serial.begin(9600);                        // Serial port init at 9600 baud                                
 } 
  
 void loop() 
 { 
-  mode = getMode();
+  mode = getMode();            // Read the mode 
 
-  if (mode==man) {
-    val = readPot();
+  if (mode==man) {            // If the mode is manual then read the pot
+    val = readPot();          // Read pot and put it in val
   }
   
-  if (mode == pwm) {    
-    val = readPWM(); 
-    Serial.print("PWM: ");
-    Serial.println(val);
-    val = map(val, 0, 10000, 179, 0) ;  
+  if (mode == pwm) {          // If the mode is pwm then read the pulse from the computer
+    val = readPWM();          //  read the pulse width and put it in val
+    //Serial.print("PWM: ");     // Simple serial debugging to read pulse width
+    //Serial.println(val);
+    val = map(val, 0, 10000, 179, 0) ;  // The pulse width can be up to 10000uS.  Map it to degrees for servo
   }
-  Serial.print("C: ");
-  Serial.println(val);
+  
     
-  if (val != lastVal) {
-    setPosition(val) ;
-    lastVal = val;
-  }
-  
-  
+  if (val != lastVal) {       // If the value has changed then update the servo
+    Serial.print("Servo Value: ");        // Send remapped value out the serial port
+    Serial.println(val);        // Send the value
+    setPosition(val) ;        // Set the servo position    
+    lastVal = val;            // reset to last state
+  } 
 } 
 
-void setPosition(int pos) {
-  speedServo.write(pos);               // set servo position
-  delay(15);                          // waits for the servo to get there   
+
+  
+
+void setPosition(int pos) {            // Set the servo position
+  speedServo.write(pos);               // Write the position to the servo
+  delay(15);                           // waits for the servo to get there   
 }
 
-unsigned long readPWM() {
-  unsigned long pw = pulseIn(pwmInputPin, HIGH);
-  return pw;
+unsigned long readPWM() {              // Read pulse from the computer
+  unsigned long pw = pulseIn(pwmInputPin, HIGH);  // Read pulse width 
+  return pw;                            // return the pulse width
 }
 
-int readPot() {
+int readPot() {                        // Read value of manual speed knob
   val = analogRead(potPin);            // reads the value of the potentiometer (value between 0 and 1023) 
   val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
   return val;
 }
 
-int getMode() {
-  mode = digitalRead(modeSwitch);
-  digitalWrite(ledPin, mode);
-  if (mode != lastMode) {    
-    Serial.println(mode);
-    lastMode = mode;
+int getMode() {                        // Read mode switch.  Either Manual or PWM
+  mode = digitalRead(modeSwitch);      // Read the value of the switch
+  digitalWrite(ledPin, mode);          // Set the debugging LED to the mode state
+  if (mode != lastMode) {              // If the mode changes then send it out serial port
+    Serial.println(mode);              // Send mode value to computer
+    lastMode = mode;                   // Reset lastmode to current mode
     
   }
-  return mode;
+  return mode;                         // Return the mode
 }
